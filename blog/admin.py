e01bac6f25f16ee3cd005dad8768644e285eb43e
samuherre <samuherre021@gmail.com>
2026-03-10 15:07:49 -0500

    chore: register domain models in Django admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from blog.domain.models import Usuario, Producto, KitEspecializado, ReservaKit
 
-# Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Producto)
+admin.site.register(KitEspecializado)
+admin.site.register(ReservaKit)
